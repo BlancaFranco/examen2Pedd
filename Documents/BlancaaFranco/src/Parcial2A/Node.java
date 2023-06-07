6eb29a49c2eb6c035f8e66a4e848c92eb5b73a9b
BlancaFranco <111582795+BlancaFranco@users.noreply.github.com>
2023-06-06 20:09:44 -0400

    Update Node.java

--- Documents/BlancaaFranco/src/Parcial2A/Node.java
+++ Documents/BlancaaFranco/src/Parcial2A/Node.java
@@ -1,13 +1,10 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * 06-06-2023
+ *Blanca Franco
  */
 package Parcial2A;
 
-/**
- *
- * @author fpuna
- */
+
 public class Node<T>{
     private T dato;
     private Node sigte;
